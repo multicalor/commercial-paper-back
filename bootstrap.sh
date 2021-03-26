b2c9016d2f2bcb5f398844e25c2d77e4508df912
multicalor <d.tretiuk@gmil.com>
2021-03-26 16:00:27 +0200

    add ti bootstrap.sh get docker function

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,23 @@ CA_VERSION=1.4.9
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
 MARCH=$(uname -m)
 
+dockerPull() {
+    #three_digit_image_tag is passed in, e.g. "1.4.7"
+    three_digit_image_tag=$1
+    shift
+    #two_digit_image_tag is derived, e.g. "1.4", especially useful as a local tag for two digit references to most recent baseos, ccenv, javaenv, nodeenv patch releases
+    two_digit_image_tag=$(echo "$three_digit_image_tag" | cut -d'.' -f1,2)
+    while [[ $# -gt 0 ]]
+    do
+        image_name="$1"
+        echo "====> hyperledger/fabric-$image_name:$three_digit_image_tag"
+        docker pull "hyperledger/fabric-$image_name:$three_digit_image_tag"
+        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name"
+        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name:$two_digit_image_tag"
+        shift
+    done
+}
+
 # This will download the .tar.gz
 download() {
     local BINARY_FILE=$1
@@ -43,6 +60,32 @@ pullBinaries() {
         echo "------> ${CA_TAG} fabric-ca-client binary is not available to download  (Available from 1.1.0-rc1) <----"
         echo
         exit
+    fi
+}
+
+pullDockerImages() {
+    command -v docker >& /dev/null
+    NODOCKER=$?
+    if [ "${NODOCKER}" == 0 ]; then
+        FABRIC_IMAGES=(peer orderer ccenv tools)
+        case "$VERSION" in
+        2.*)
+            FABRIC_IMAGES+=(baseos)
+            shift
+            ;;
+        esac
+        echo "FABRIC_IMAGES:" "${FABRIC_IMAGES[@]}"
+        echo "===> Pulling fabric Images"
+        dockerPull "${FABRIC_TAG}" "${FABRIC_IMAGES[@]}"
+        echo "===> Pulling fabric ca Image"
+        CA_IMAGE=(ca)
+        dockerPull "${CA_TAG}" "${CA_IMAGE[@]}"
+        echo "===> List out hyperledger docker images"
+        docker images | grep hyperledger
+    else
+        echo "========================================================="
+        echo "Docker not installed, bypassing download of Fabric images"
+        echo "========================================================="
     fi
 }
 
@@ -80,3 +123,4 @@ echo
 echo "Pull Hyperledger Fabric binaries"
 echo
 pullBinaries
+pullDockerImages
